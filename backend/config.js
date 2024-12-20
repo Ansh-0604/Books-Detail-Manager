00496b79d222e1b78f98c2c914c93e7024bb9ffb
AnshBadal_0604 <122770499+Ansh-0604@users.noreply.github.com>
2024-12-20 12:23:28 +0530

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,2 +0,0 @@
-export const PORT = 5555;
-export const mongoDBURL = "mongodb+srv://root:root@books-store-mern.qjrob.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"
